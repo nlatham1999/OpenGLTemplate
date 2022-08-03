1c59eed2449ab41befbb7a36a86883bde7d05896
nlatham1999 <nicholaslatham1999@gmail.com>
2022-08-02 22:09:22 -0700

    changes

--- OpenGLTemplate/Main.cpp
+++ OpenGLTemplate/Main.cpp
@@ -49,10 +49,21 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     //set vertext data
+    //float vertices[] = {
+    //-0.5f, -0.5f, 0.0f,
+    // 0.5f, -0.5f, 0.0f,
+    // 0.0f,  0.5f, 0.0f
+    //};
+
     float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
 
     //set the buffer
@@ -62,11 +73,20 @@ int main()
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
 
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    // 1. bind Vertex Array Object
     glBindVertexArray(VAO);
+    // 2. copy our vertices array in a vertex buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 4. then set the vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     //create and load the vertext shader
     unsigned int vertexShader;
@@ -91,10 +111,8 @@ int main()
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-
+    //line mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -105,7 +123,10 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
